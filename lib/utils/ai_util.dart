ad745b39d751cd4e82396463ab76d1c4cb7cd40d
Nikhil <97503662+nikhilhvr@users.noreply.github.com>
2022-06-06 20:07:50 +0530

    Create ai_util.dart

---
+++ lib/utils/ai_util.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+mixin AIColors {
+  static const Color primaryColor1 = Vx.black;
+  static const Color primaryColor2 = Color(0xFF32A7FF);
+}
